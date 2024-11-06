477a7e1986a10727cb575c304bfad6974df0123a
Nico Joun <nicojoun793@gmail.com>
2024-11-06 12:25:06 +0100

    la page à propos affiche les langages

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,31 +1,30 @@
 import Banner from '../../components/Banner';
-import Collapse from '../../components/Collapse';
+import Card from '../../components/Card';
 import '../../assets/styles/About.scss';
 
-const dataCollapse = [
-  { title: 'Foutez-moi la paix', content: (<p>{'Si vous n’avez rien de mieux à faire, foutez-moi la paix et allez vous faire enculer !'}</p>) },
-  { title: 'Dégagez de ma vue', content: (<p>{'Si ça ne vous plaît pas, dégagez de ma vue avant que je pète un câble !'}</p>) },
-  { title: 'J’en ai ras-le-bol', content: (<p>{'Vos conneries, j’en ai ras-le-bol ! Gardez vos avis à la con pour vous.'}</p>) },
-  { title: 'Cassez-vous, bande de nazes', content: (<p>{'Si vous êtes pas contents, cassez-vous, bande de nazes !'}</p>) }
+const dataCard = [
+  { title: 'HTML', imageSrc: require('../../assets/image/image_html.png') },  // Utilisation de `require` pour les images locales
+  { title: 'CSS', imageSrc: require('../../assets/image/image_css.png') },  // Utilisation de `require` pour les images locales
+  { title: 'JS', imageSrc: require('../../assets/image/image_js.png') },  // Utilisation de `require` pour les images locales
+  { title: 'React', imageSrc: require('../../assets/image/image_react.png') } // Utilisation de `require` pour les images locales
 ];
-
 
 function About() {
   return (
     <div className='about'>
       <Banner bannerClass='about-banner' />
-      {/* Boucle pour afficher chaque élément du tableau dans un Collapse */}
-      <div className='about-collapse'>
-      {dataCollapse.map((item, index) => (
-        <Collapse 
-          key={index} 
-          title={item.title} 
-          content={item.content} 
-        /> 
-      ))}
+      {/* Boucle pour afficher des cards */}
+      <div className='about-cards'>
+        {dataCard.map((item, index) => (
+          <Card 
+            key={index} 
+            cardTitle={item.title} 
+            cardSrc={item.imageSrc} 
+          />  
+        ))}
       </div>
     </div>
   );
-} 
+}
 
 export default About;

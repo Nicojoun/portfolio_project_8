1731153a4273db03aa051bf7947cb43967bf343d
Nico Joun <nicojoun793@gmail.com>
2024-11-18 14:03:24 +0100

    optimisation

--- src/pages/Languages/index.jsx
+++ src/pages/Languages/index.jsx
@@ -3,11 +3,11 @@ import Card from '../../components/Card';
 import '../../assets/styles/Languages.scss';
 
 const dataCard = [
-  { title: 'HTML', imageSrc: require('../../assets/image/image_html.webp') }, 
-  { title: 'CSS', imageSrc: require('../../assets/image/image_css.webp') },  
-  { title: 'JS', imageSrc: require('../../assets/image/image_js.webp') },  
-  { title: 'React', imageSrc: require('../../assets/image/image_react.webp') },
-  { title: 'Node.js', imageSrc: require('../../assets/image/image_node_JS.webp') }
+  { title: 'HTML', imageSrc: require('../../assets/image/image_html.webp'), alt: 'Icône HTML' }, 
+  { title: 'CSS', imageSrc: require('../../assets/image/image_css.webp'), alt: 'Icône CSS' },  
+  { title: 'JS', imageSrc: require('../../assets/image/image_js.webp'), alt: 'Icône JavaScript' },  
+  { title: 'React', imageSrc: require('../../assets/image/image_react.webp'), alt: 'Icône React' },
+  { title: 'Node.js', imageSrc: require('../../assets/image/image_node_JS.webp'), alt: 'Icône Node.js' }
 ];
 
 function Languages() {
@@ -21,6 +21,7 @@ function Languages() {
             key={index} 
             cardTitle={item.title} 
             cardSrc={item.imageSrc} 
+            cardAlt={item.alt} 
           />  
         ))}
       </div>
